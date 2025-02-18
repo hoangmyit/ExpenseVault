ee32fd66ae17af63dc5b059f36016132740f9662
My-Hoang <hoangmyit@outlook.com>
2025-02-18 22:38:31 +0700

    Add depency injection

---
+++ EV.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EV.Application
+{
+  public static class DependencyInjection
+  {
+    public static void AddApplicationService(this IHostApplicationBuilder builder)
+    {
+      builder.Services
+        .AddAutoMapper(Assembly.GetExecutingAssembly())
+        .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+        .AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+    }
+  }
+}
