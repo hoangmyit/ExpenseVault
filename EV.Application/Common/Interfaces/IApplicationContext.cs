8c21c83c0211b1547b31edfbf1150a47d084df1d
My-Hoang <hoangmyit@outlook.com>
2025-03-15 00:37:35 +0700

    Add transaction management and entity state manipulation methods to IApplicationContext and ApplicationDbContext

--- EV.Application/Common/Interfaces/IApplicationContext.cs
+++ EV.Application/Common/Interfaces/IApplicationContext.cs
@@ -11,5 +11,13 @@ namespace EV.Application.Common.Interfaces
         DbSet<CategoryGroup> CategoryGroups { get; }
         DbSet<Expense> Expenses { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        int SaveChanges();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+        Task<int> ExecuteSqlRawAsync(string sql, params object[] parameters);
+        void SetEntityState<TEntity>(TEntity entity, EntityState state) where TEntity : class;
+        void AttachEntity<TEntity>(TEntity entity) where TEntity : class;
+        IQueryable<TEntity> GetAsNoTracking<TEntity>() where TEntity : class;
     }
 }
